7b98a1435eaf28adaf636cdb21c13636c1686563
projetotransforma <equipe.projetotransforma@gmail.com>
2025-11-25 22:15:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,8 @@ body, .stApp, .block-container, label, p, h1, h2, h3, h4, h5, h6 {color: black !
 # ------------------------------ # Banner com logo # ------------------------------
 banner_html = """
 <div style="width:100%; height:300px; position:relative; background-color:#f1e3d8;">
-<img src="https://raw.githubusercontent.com/leticiabrantendocrinologia/efca-app/bf9fca05f3ee47c7425829cc2ebd26733e93b0d8/logo.png"
+<img src="https://raw.githubusercontent.com/SEU_USUARIO/NOME_REPO/BRANCH/logotransforma.png
+"
      style="position:absolute; top:45%; left:50%; transform:translate(-50%, -45%); height:220px;">
 </div>
 """
